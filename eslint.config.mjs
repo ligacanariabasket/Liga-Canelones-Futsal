442e87232ca4e1b18aacb5583d14cb133eb2337a
Liga Canaria <ligacanariabasket@gmail.com>
2025-09-30 12:33:14 +0000

    Evita usar JavaScript antiguo en navegadores modernos Ahorro potencial d

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
