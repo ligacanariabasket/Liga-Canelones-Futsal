5b62b877cb65b20fe5f7f02b7eb14cc122cbad71
Liga Canaria <ligacanariabasket@gmail.com>
2025-09-30 09:45:50 +0000

    es

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
         port: '',
-        pathname: '/**',
+      pathname: '/**',
       },
       {
         protocol: 'https',
