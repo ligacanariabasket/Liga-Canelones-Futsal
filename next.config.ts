5126f9fcf5d3c6e31b75f374c836c811505ea75e
Liga Canaria <ligacanariabasket@gmail.com>
2025-09-30 12:17:16 +0000

    ner

---
+++ next.config.ts
@@ -0,0 +1,84 @@
+
+import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
+
+const pwaConfig = {
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development' || !!process.env.TURBOPACK,
+};
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  allowedDevOrigins: [
+      '6000-*.cloudworkstations.dev',
+      '9000-*.cloudworkstations.dev'
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ligacanelonesfutsalmc.netlify.app',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https'
+        ,
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverExternalPackages: ['handlebars','@genkit-ai/googleai', 'genkit'],
+
+  async redirects() {
+    return [
+      {
+        source: '/partidos-viejos',
+        destination: '/partidos',
+        permanent: true, // This generates a 301 redirect
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default withPWA(pwaConfig)(nextConfig);
