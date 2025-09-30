3199b1c0a302bc96775f4283b6cb7929b264f0e1
Liga Canaria <ligacanariabasket@gmail.com>
2025-09-30 10:37:46 +0000

    [{ 	"resource": "/home/user/studio/src/lib/prisma.ts", 	"owner": "typesc

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,15 +3,17 @@ import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
-  // allow global `var` declarations
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+  var prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
-export const prisma =
-  global.prisma ||
+const createPrismaClient = () =>
   new PrismaClient({
-    log: ['query'],
+    log: [],
   }).$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+export const prisma = global.prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
